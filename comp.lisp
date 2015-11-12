edea1466860153cf1058c62bfa4b1a93bcba7902
bowei liu <liubowei@gmail.com>
2015-11-11 22:15:11 -0800

    startin new branch

--- comp.lisp
+++ comp.lisp
@@ -6,15 +6,6 @@
    ( + ( * x x ) (* x z ) (* z z) )
    ( * a c (+ a c ) )
    ( * x z (+ x z ) )))
-(defun fuzz-val () 1.0d-8)
-(defun leq2 (e1 e2)
-  (< e1 (+ e2 (fuzz-val))))
-(defun leq (s1 &rest s2)
-  (cond ((null s2) t)
-	(t (and
-	    (leq2 s1 (car s2))
-	    (apply 'leq s2)))))
-
 (defun constraints (eps)
   (lambda (a c x z) ( and
     (leq a 0 c)
@@ -27,19 +18,25 @@
     (leq (+ c z) (- (/ 4 3) (* 4 eps)))
     (leq (+ 1 a) z))))
 
-(defun gen-range (bot top step)
-  (cond
-    ((not (leq bot top)) ())
-    (t
-     (cons bot (gen-range (+ bot step) top step)))))
+(defun fuzz-val () 1.0d-8)
+(defun leq2 (e1 e2) (< e1 (+ e2 (fuzz-val))))
+(defun leq (s1 &rest s2)
+  (cond ((null s2) t)
+	(t (and	(leq2 s1 (car s2))
+	        (apply 'leq s2)))))
 
-(map 'list #' (lambda (fir) (map 'list #' (lambda (x) (cons fir x)) (gen-range 0 10 1))) (gen-range 0 10 1))
+(defun gen-range (bot top step)
+  (cond ((not (leq bot top)) ())
+	(t (cons bot (gen-range (+ bot step) top step)))))
+
+;(map 'list #' (lambda (fir) (map 'list #' (lambda (x) (cons fir x)) (gen-range 0 10 1))) (gen-range 0 10 1))
 
 (defun range (step-sz)
   '(( -10 0)
    (c 0 10)
    (z 0 10)
    (x -10 0)))
+
 
 
 
